8f24a68c90efb2a654018bbf4e297a3925681524
JulioF127 <juliofuentes12@hotmail.com>
2021-06-04 02:13:45 -0600

    Si.

--- app/src/main/java/com/example/necflis/busqueda.java
+++ app/src/main/java/com/example/necflis/busqueda.java
@@ -3,6 +3,12 @@ package com.example.necflis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class busqueda extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class busqueda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda);
+
+        ImageButton imageButton = findViewById(R.id.imageButton8);
+       imageButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                       busqueda.this, R.style.BottomSheetDialogTheme
+               );
+               View bottomSheetView = LayoutInflater.from(getApplicationContext())
+                       .inflate(
+                               R.layout.layout_buttom_sheet,
+                               (LinearLayout)findViewById(R.id.buttonSheetContainer)
+                       );
+           }
+       });
     }
 }
